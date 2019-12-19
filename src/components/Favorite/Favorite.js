369e3d051bd6c13b7bd16e1fd3b2b434af141f1f
Elmi Elmi <elmielmi522@gmail.com>
2019-12-19 16:53:31 -0600

    Add category radio btns

--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -3,17 +3,76 @@ import { connect } from 'react-redux';
 
 class Favorite extends Component {
 
+    state = {
+        id: '',
+        category: ''
+    }
+
     componentDidMount() {
         this.props.dispatch( { type: 'GET_FAVORITE' } )
     }
 
+    handleChange = (event) => {
+        this.setState({
+            category: event.target.value
+        })
+    }
+
     render() {
         return (
-            <ul>
+            <div>
+                <div>
+                    <label>
+                        Vega
+                        <input type='radio'
+                        name='radio'
+                        id='vega'
+                        value='vega'
+                        check={this.state.category === 2}
+                        onChange={(event)=> this.handleChange(event)}
+                        />
+                        <br/>
+                        Cartoon
+                        <input type='radio'
+                            name='radio'
+                            id='cartoon'
+                            value='cartoon'
+                            check={this.state.category === 3}
+                            onChange={(event)=> this.handleChange(event)}
+                        />
+                        <br/>
+                        Meme
+                        <input type='radio'
+                            name='radio'
+                            id='meme'
+                            value='meme'
+                            check={this.state.category === 5}
+                            onChange={(event)=> this.handleChange(event)}
+                        />
+                        <br/>
+                        NSFW
+                        <input type='radio'
+                            name='radio'
+                            id='nsfw'
+                            value='nsfw'
+                            check={this.state.category === 4}
+                            onChange={(event)=> this.handleChange(event)}
+                        />
+                        <br/>
+                        Funny
+                        <input type='radio'
+                            name='radio'
+                            id='funny'
+                            value='funny'
+                            check={this.state.category === 1}
+                            onChange={(event)=> this.handleChange(event)}
+                        />
+                </label>
+                </div>
                 {this.props.reduxState.map((item, i) => 
-                    <li key={i}>{item.name}</li>
+                    <p key={i}>{item.name}</p>
                 )}
-            </ul>
+            </div>
         );
     }
 }
