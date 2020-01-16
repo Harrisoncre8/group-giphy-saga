6040a3f96ad0193152595a0cb23e8f5a8a34aa74
Harrison Nguyen <nguy2173@umn.edu>
2020-01-16 14:02:36 -0600

    Optimize code

--- server/routes/category.router.js
+++ server/routes/category.router.js
@@ -6,19 +6,6 @@ const dotenv = require(`dotenv`);
 const router = express.Router();
 dotenv.config();
 
-// router.get('/', (req, res) => {
-//     // return all categories
-//     const queryText = `SELECT * FROM category ORDER BY name ASC`;
-//     pool.query(queryText)
-//         .then( (result) => {
-//             res.send(result.rows);
-//         })
-//         .catch( (error) => {
-//             console.log(`Error on query ${error}`);
-//             res.sendStatus(500);
-//         });
-// });
-
 router.get('/:id', (req, res) => {
     console.log('-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------');
     let apiKey = process.env.API_KEY;
